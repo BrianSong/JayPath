1c271cf8508a969ede1192f5ce8d7e343fdc53f5
BrianSong <briansong0122@gmail.com>
2020-02-26 15:59:07 -0500

    Commit for database section

--- express/index.js
+++ express/index.js
@@ -7,27 +7,7 @@ const sqlite3 = require("sqlite3").verbose();
 app.use(express.json());
 app.use(cors());
 
-// open the database
-let db = new sqlite3.Database("../db/JayPath.db", err => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log("Connected to the courses database for initilization!");
-});
-
-db.run(
-  "CREATE TABLE IF NOT EXISTS courses(id INTEGER NOT NULL PRIMARY KEY, CourseNumber TEXT, CourseTitle TEXT, Credits INTEGER, Instructor TEXT, DaysOfWeek TEXT, StartTimeEndTime TEXT, Track TEXT)"
-);
-
-buildDB();
-
-// close the database
-db.close(err => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log("Close the courses database connection for initilization!");
-});
+initilization();
 
 let courses = [];
 
@@ -45,11 +25,6 @@ app.get("/api/test", (req, res) => {
 
 app.get("/api/:field/courses", (req, res) => {
   const field = String(req.params.field);
-  //  for (let i = 0; i < courses.length; i++){
-  //      if(courses[i].field == field){
-  //           res.send(courses[i])
-  //      }
-  //  }
 
   // Open database
   let db = new sqlite3.Database("../db/JayPath.db", err => {
@@ -61,7 +36,7 @@ app.get("/api/:field/courses", (req, res) => {
 
   // Extract course according to the focus area and sent it back to the front end for displaying.
   let sql = `SELECT * FROM courses WHERE Track = ?;`;
-  db.all(sql, [String(field)], (err, course) => {
+  db.all(sql, [field], (err, course) => {
     if (err) {
       throw err;
     }
@@ -76,7 +51,6 @@ app.get("/api/:field/courses", (req, res) => {
       console.error(err.message);
     }
     console.log("Close the courses connection.");
-
     res.send(courses);
   });
 });
@@ -134,33 +108,52 @@ function validateCourse(course) {
   return Joi.validate(course, schema);
 }
 
-function buildDB() {
+function initilization() {
+  // open the database
+  let db = new sqlite3.Database("../db/JayPath.db", err => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the courses database for initilization!");
+  });
+
+  db.run(
+    "CREATE TABLE IF NOT EXISTS courses(id INTEGER NOT NULL PRIMARY KEY, CourseNumber TEXT, CourseTitle TEXT, Credits INTEGER, Instructor TEXT, DaysOfWeek TEXT, StartTimeEndTime TEXT, Track TEXT)"
+  );
+
   // hardcode for the first iteration: add courses manually
   // plan to retrive courses from SIS API in the following iteration
 
   // Track: big data, computational biology, nlp, Robotics, Information securities
   let courseInfo = [
-     [0, "601", "OOSE", 3, "Avi", "TT", "12", "bigdata"],
-     [1, "601", "databases", 3, "Sara", "TT", "9", "bigdata"]
+    [0, "601", "OOSE", 3, "Avi", "TT", "12", "bigdata"],
+    [1, "601", "databases", 3, "Sara", "TT", "9", "bigdata"]
   ];
 
   // create the statement for the insertion of just ONE record
-  let queryInfo = 
-     "INSERT OR REPLACE INTO courses(id, CourseNumber, CourseTitle, Credits, Instructor, DaysOfWeek, StartTimeEndTime, Track) " +
-     "VALUES (?, ?, ? ,?, ?, ?, ? ,?)";
+  let queryInfo =
+    "INSERT OR REPLACE INTO courses(id, CourseNumber, CourseTitle, Credits, Instructor, DaysOfWeek, StartTimeEndTime, Track) " +
+    "VALUES (?, ?, ? ,?, ?, ?, ? ,?)";
 
-  // 'prepare' returns a 'statement' object which allows us to 
+  // 'prepare' returns a 'statement' object which allows us to
   // bind the same query to different parameters each time we run it
   let statement = db.prepare(queryInfo);
 
   // run the query over and over for each inner array
   for (var i = 0; i < courseInfo.length; i++) {
-      statement.run(courseInfo[i], function (err) {
-          if (err) throw err;
-      });
+    statement.run(courseInfo[i], function(err) {
+      if (err) throw err;
+    });
   }
   statement.finalize();
 
+  // close the database
+  db.close(err => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Close the courses database connection for initilization!");
+  });
 }
 
 // PORT
