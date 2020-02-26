be3eb29b54dc1384c6d5490fd00648d34e0ad32b
BrianSong <briansong0122@gmail.com>
2020-02-26 16:28:36 -0500

    database finish 1st iteration

--- express/index.js
+++ express/index.js
@@ -126,8 +126,156 @@ function initilization() {
 
   // Track: big data, computational biology, nlp, Robotics, Information securities
   let courseInfo = [
-    [0, "601", "OOSE", 3, "Avi", "TT", "12", "bigdata"],
-    [1, "601", "databases", 3, "Sara", "TT", "9", "bigdata"]
+    [
+      0,
+      "EN.601.315",
+      "Databases",
+      3,
+      "D. Yarowsky",
+      "TTh",
+      "3:00PM - 4:15PM",
+      "big_data"
+    ],
+    [
+      1,
+      "EN.601.434",
+      "Randomized and Big Data Algorithms",
+      3,
+      "V. Braverman",
+      "TTh",
+      "12:00PM - 1:15PM",
+      "big_data"
+    ],
+    [
+      2,
+      "EN.601.419",
+      "Cloud Computing",
+      3,
+      "S. Ghorbani Khaledi",
+      "MW",
+      "12:00PM - 1:15PM",
+      "big_data"
+    ],
+    [
+      3,
+      "EN.601.350",
+      "Genomic Data Science",
+      3,
+      "S. Salzberg",
+      "TTh",
+      "3:00PM - 4:15PM",
+      "computational_biology"
+    ],
+    [
+      4,
+      "EN.601.455",
+      "Computer Integrated Surgery",
+      3,
+      "R. Taylor",
+      "TTh",
+      "1:30PM - 2:45PM",
+      "computational_biology"
+    ],
+    [
+      5,
+      "EN.601.447",
+      "Computational Genomics: Sequences",
+      3,
+      "B. Langmead",
+      "TTh",
+      "12:00PM - 1:15PM",
+      "computational_biology"
+    ],
+    [
+      6,
+      "EN.601.464",
+      "Artificial Intelligence",
+      3,
+      "J. Sedoc, B. Van Durme",
+      "TTh",
+      "9:00AM - 10:15AM",
+      "nlp"
+    ],
+    [
+      7,
+      "EN.601.465",
+      "Natural Language Processing",
+      3,
+      "K. Duh",
+      "MWF",
+      "11:00AM - 11:50AM",
+      "nlp"
+    ],
+    [
+      8,
+      "EN.601.475",
+      "Machine Learning",
+      3,
+      "P. Graff",
+      "MWF",
+      "4:30PM - 5:45PM",
+      "nlp"
+    ],
+    [
+      9,
+      "EN.601.463",
+      "Algorithms for Sensor-Based Robotics",
+      3,
+      "S. Leonard",
+      "TTh",
+      "4:30PM - 5:45PM",
+      "robotics"
+    ],
+    [
+      10,
+      "EN.601.455",
+      "Computer Integrated Surgery I",
+      4,
+      "R. Taylor",
+      "TTh",
+      "1:30PM - 2:45PM",
+      "robotics"
+    ],
+    [
+      11,
+      "EN.601.461",
+      "Computer Vision",
+      3,
+      "G. Hager",
+      "TTh",
+      "9:00AM - 10:15AM",
+      "robotics"
+    ],
+    [
+      12,
+      "EN.650.672",
+      "Security Analytics",
+      3,
+      "L. Ding",
+      "M",
+      "6:00PM - 8:30PM",
+      "information_securities"
+    ],
+    [
+      13,
+      "EN.650.663",
+      "Cloud Computing Security",
+      3,
+      "C. Monson",
+      "F",
+      "1:30PM - 4:00PM",
+      "information_securities"
+    ],
+    [
+      14,
+      "EN.650.624",
+      "Network Security",
+      3,
+      "R. Johnston",
+      "MW",
+      "3:00PM - 4:15PM",
+      "information_securities"
+    ]
   ];
 
   // create the statement for the insertion of just ONE record
