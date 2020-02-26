d1849ffa7ff40052548fa8ce2ad9607cd61295fb
rbao2 <rbao2@jhu.edu>
2020-02-26 15:31:14 -0500

    finished frontend iteration one

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
-    constructor(prop) {
-        super(prop);
+    constructor(props) {
+        super(props);
         this.state = {
+            question: "What is your focus area in computer science?",
             apiResponse: "",
-            number: ""
+            number: "",
+            data: []
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     callAPI() {
@@ -36,18 +45,241 @@ class App extends Component {
         event.preventDefault();
     }
 
+    handleClick(event) {
+        
+    }
+
+
+
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" onChange={this.handleChange}/>
-                </label>
-                <p>{this.state.apiResponse}</p>
-                <input type="submit" value="Submit" />
-            </form>
+            <Router>
+        
+        <Switch>
+          <Route exact path="/">
+          <div>
+            <h1 style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                }}>
+                {this.state.question}
+            </h1>
+            <div style={{ 
+                marginLeft: "auto", 
+                display: "flex", 
+                justifyContent: "center",}}>
+                <Link to="/r"><button class="button1" type="button">Robotics</button></Link>
+                <Link to="/bd"><button class="button4" type="button">Big Data</button></Link>
+                <Link to="/is"><button class="button5" type="button">Information Security</button></Link>
+                <Link to="/cb"><button class="button3" type="button">Computational Biology</button></Link>
+                <Link to="/nlp"><button class="button2" type="button">Natural Language Processing</button></Link>
+            </div>
+        </div>
+          </Route>
+          <Route path="/nlp">
+            <NLP />
+          </Route>
+          <Route path="/r">
+            <ROB />
+          </Route>
+          <Route path="/bd">
+            <BD />
+          </Route>
+          <Route path="/is">
+            <IS />
+          </Route>
+          <Route path="/cb">
+            <CB />
+          </Route>
+        </Switch>
+        </Router>
         );
     }
+}
+
+
+class NLP extends Component {
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            apiResponse: "",
+        };
+    }
+
+    callAPI() {
+        fetch('http://localhost:5000/api/test')
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}))
+            .catch(err => err)
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+
+    render() {
+        return (
+            <div>
+            <h1 style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                }}>
+                Welcome to Natural Language Processing!
+            </h1>
+            </div>
+        );
+    }
+
+}
+
+class CB extends Component {
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            apiResponse: "",
+        };
+    }
+
+    callAPI() {
+        fetch('http://localhost:5000/api/test')
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}))
+            .catch(err => err)
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+
+    render() {
+        return (
+            <div>
+            <h1 style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                }}>
+                Welcome to Computational Biology!
+            </h1>
+            </div>
+        );
+    }
+
+}
+
+class IS extends Component {
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            apiResponse: "",
+        };
+    }
+
+    callAPI() {
+        fetch('http://localhost:5000/api/test')
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}))
+            .catch(err => err)
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+
+    render() {
+        return (
+            <div>
+            <h1 style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                }}>
+                Welcome to Information Securities!
+            </h1>
+            </div>
+            
+        );
+    }
+
+}
+
+class ROB extends Component {
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            apiResponse: "",
+        };
+    }
+
+    callAPI() {
+        fetch('http://localhost:5000/api/test')
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}))
+            .catch(err => err)
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+
+    render() {
+        return (
+            <div>
+            <h1 style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                }}>
+                Welcome to Robotics!
+            </h1>
+            </div>
+            
+        );
+    }
+
+}
+
+class BD extends Component {
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            apiResponse: "",
+        };
+    }
+
+    callAPI() {
+        fetch('http://localhost:5000/api/test')
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}))
+            .catch(err => err)
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+
+    render() {
+        return (
+            <div>
+            <h1 style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                }}>
+                Welcome to Big Data!
+            </h1>
+            </div>
+            
+        );
+    }
+
 }
 
 export default App;
