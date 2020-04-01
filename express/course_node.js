c57f976fa59b4280bb3ea4a56a8e4db1a3d25289
Long Wang <wanglong24@gmail.com>
2020-03-31 18:43:39 -0700

    new course_node class

---
+++ express/course_node.js
@@ -0,0 +1,42 @@
+class course_node {
+  constructor(course_status) {
+    this.status = course_status;
+    this.value = 0;
+    this.child_indices = [];
+    this.best_child_index = -1;
+  }
+
+  // getter
+  get get_status() {
+  	return this.status;
+  }
+
+  get get_value() {
+  	return this.value;
+  }
+
+  get get_child_indices() {
+  	return this.child_indices;
+  }
+
+  get get_best_child_index() {
+  	return this.best_child_index;
+  }
+
+  // setter
+  set set_status(course_status) {
+  	this.status = course_status;
+  }
+
+  set set_value(node_value) {
+  	this.value = node_value;
+  }
+
+  set add_child_index(index) {
+  	this.child_indices.push(index);
+  }
+
+  set set_best_child_index(index) {
+  	this.best_child_index = index;
+  }
+}
