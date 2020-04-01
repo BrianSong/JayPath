e284e40fde1139f3b302642cd88dba7a40b8fe69
Long Wang <wanglong24@gmail.com>
2020-03-31 22:36:27 -0700

    finish get no conflict function

--- express/get_one_semester.js
+++ express/get_one_semester.js
@@ -6,8 +6,6 @@ module.exports = {
         // e.g.: [node(0,0,0,0)]
         // output: list of course nodes at the current semester
         // e.g.: [node(1,1,0,0), node(0,1,1,0)]
-        let child_nodes = [];
-        let child_node_idx = 0;
 
         let curr_semester_nodes_list = []; // store the course nodes for current semester
 
@@ -15,13 +13,13 @@ module.exports = {
         for (var i = 0; i < prev_semester_nodes_list.length; i++) {
             // generate a possible course node at the current semester
             // get a list of child nodes based on a given course node
-            child_nodes = get_child_nodes(prev_semester_nodes_list[i], field)
+            let child_nodes = get_child_nodes(prev_semester_nodes_list[i], field)
 
             // add child course nodes in the current semester nodes list
             // note: make sure there is no duplicates!
             for (var j = 0; j < child_nodes.length; j++) {
                 // check if the child node is in the current semester nodes list
-                child_node_idx = curr_semester_nodes_list.findIndex(element => element == child_nodes[j])
+                let child_node_idx = curr_semester_nodes_list.findIndex(element => element == child_nodes[j])
 
                 if (child_node_idx == -1) {
                     // add a new child node into the current semester nodes list
@@ -38,16 +36,47 @@ module.exports = {
     },
 };
 
-function get_child_nodes(course_node, field) {
-    let course_status = course_node.get_status(); // e.g. [0,0,0,0]
-    return filter.filterByPre(course_status, field, get_child_from_eligible) // for function callback
+function get_child_nodes(prev_course_node, field) {
+    let prev_course_status = prev_course_node.get_status();
+    return filter.filterByPre(prev_course_status, field, get_no_conflict_courses_from_eligible) // for function callback
 }
 
-function get_child_from_eligible(eligible_courses) {
-    let child_nodes = [];
+function get_no_conflict_courses_from_eligible(prev_course_node, eligible_courses) {
+    // extract useful information
+    let eligible_ids = [] // course id for each eligible course, e.g.: [1,2,5]
+    let conflict_ids = [] // course id of conflict courses for each eligible course, e.g.: [[2,3], [1], []]
     for (var i = 0; i < eligible_courses.length; i++) {
-        eligible_courses[i]
+        eligible_ids.push(eligible_courses[i].id);
+        let conflict_string = eligible_courses[i].Conflict;
+        conflict_ids.push(conflict_string.toString().split("-"));
     }
-    
+
+    let child_nodes = [];
+    // select no conflict courses based on just ids
+    for (var i = 0; i < eligible_ids.length; i++) {
+        let course1_id = eligible_ids[i];
+        
+        for (var j = i+1; i < eligible_ids.length; j++) {
+            let course2_id = eligible_ids[j];
+            if (conflict_ids[i].includes(course2_id)) {
+                continue;
+            }
+            
+            for (var k = j+1; k < eligible_ids.length; k++) {
+                let course3_id = eligible_ids[k];
+                if (conflict_ids[j].includes(course3_id)) {
+                    continue
+                } else {
+                    let child_course_status = prev_course_node.get_status();
+                    child_course_status[course1_id] = 1;
+                    child_course_status[course2_id] = 1;
+                    child_course_status[course3_id] = 1;
+                    let child_node = new course_node(child_course_status);
+                    child_nodes.push(child_node);
+                }
+            }
+        }
+    }    
+
     return child_nodes;
 }
