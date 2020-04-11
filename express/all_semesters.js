85d8bcc23a38a405efacfe8d77872bf7048348b0
Long Wang <wanglong24@gmail.com>
2020-04-11 15:41:21 -0700

    add old files

---
+++ express/all_semesters.js
@@ -0,0 +1,62 @@
+const one_semester = require("./one_semester");
+
+module.exports = {
+    get_all_semesters: function get_all_semesters(user_semester, field) {
+        // input: user course nodes list (a list contains a single course node)
+        // output: a list where each element (corresponding to a term) is a list of course codes
+
+        let all_semesters = []; // every element is a list of courses for a term
+
+        // get the very next semester nodes based on the user nodes list
+        let curr_semester_nodes_list = one_semester.get_one_semester(user_semester, field);
+        
+        // note: check graduation before push to all semesters to make sure values are assigned
+        // check graudation and assign values (pass by reference in order to modify input?)
+        // let grad_flag = check_grad(curr_semester_nodes_list, field); // not used for iter3
+        let grad_flag = false;
+
+        // add the current semester nodes list into the all semesters list
+        all_semesters.push(curr_semester_nodes_list);
+
+        while (!grad_flag) { // not graduate yet, need to take one more semester
+            // get the next semester nodes
+            curr_semester_nodes_list = one_semester.get_one_semester(all_semesters[-1], field); // only need to input the most recent semester
+
+            // check graudation and assign values (pass by reference in order to modify input?)
+            // grad_flag = check_grad(curr_semester_nodes_list, field); // not used for iter3
+
+            // add the current semester nodes list into the all semesters list
+            all_semesters.push(curr_semester_nodes_list);
+
+            if (all_semesters.length == 8) { break; } // only for iter3!!!
+       }
+
+       return all_semesters; // note all the nodes should have values assinged, but only the last semester has value 1 and matters
+    },
+};
+
+function check_grad(nodes_list, field){
+    let grad_flag = false;
+    let curr_node_status = [];
+
+    // looping over all course nodes
+    for (var i = 0; i < nodes_list.length; i++) {
+        // assing value by checking graduation requirement, pass: 1; fail: -1
+        curr_node_status = nodes_list[i].get_status();
+        
+        // check core requirement
+
+        // check total credits requirement
+
+        // check field requirement
+
+        node_value = 0;
+        nodes_list[i].set_value() = node_value;
+
+        if (node_value == 1) {
+            grad_flag = true; // do not break since we need to assign values to all nodes
+        }
+    }
+
+    return grad_flag;
+}
---
+++ express/one_semester.js
@@ -0,0 +1,95 @@
+const filter = require("./filter");
+
+module.exports = {
+    get_one_semester: function get_one_semester(prev_semester_nodes_list, field) {
+        // input: list of course nodes at the previous semester
+        // e.g.: [node(0,0,0,0)]
+        // output: list of course nodes at the current semester
+        // e.g.: [node(1,1,0,0), node(0,1,1,0)]
+        console.log("called");
+        console.log(prev_semester_nodes_list.length);
+        let curr_semester_nodes_list = []; // store the course nodes for current semester
+
+        // looping for all course nodes in the previous semester
+        for (var i = 0; i < prev_semester_nodes_list.length; i++) {
+            console.log("entered the loop");
+            // generate a possible course node at the current semester
+            // get a list of child nodes based on a given course node
+            console.log(prev_semester_nodes_list[i].get_status);
+            let child_nodes = get_child_nodes(prev_semester_nodes_list[i], field);
+            console.log(child_nodes);
+
+            // add child course nodes in the current semester nodes list
+            // note: make sure there is no duplicates!
+            for (var j = 0; j < child_nodes.length; j++) {
+                // check if the child node is in the current semester nodes list
+                let child_node_idx = curr_semester_nodes_list.findIndex(child_nodes[j].equals);
+
+                if (child_node_idx == -1) {
+                    // add a new child node into the current semester nodes list
+                    curr_semester_nodes_list.push(child_nodes[j]);
+                    // remember the child node index
+                    // prev_semester_nodes_list[i].add_child_index(curr_semester_nodes_list.length-1); // not used for iter3
+                } else {
+                    // remember the child node index
+                    // prev_semester_nodes_list[i].add_child_index(child_node_idx);  // not used for iter3
+                }
+            }
+        }
+        return curr_semester_nodes_list;
+    }
+};
+
+function get_child_nodes(prev_course_node, field) {
+    let prev_course_status = prev_course_node.get_status;
+    return filter.filterByPre(prev_course_status, field, select_from_eligible); // for function callback
+}
+
+function select_from_eligible(prev_course_status, eligible_courses) {
+    console.log("select_from_eligible called");
+    // extract useful information
+    let eligible_ids = []; // course id for each eligible course, e.g.: [1,2,5]
+    let conflict_ids = []; // course id of conflict courses for each eligible course, e.g.: [[2,3], [1], []]
+    for (let i = 0; i < eligible_courses.length; i++) {
+        eligible_ids.push(eligible_courses[i].id);
+        // let conflict_string = eligible_courses[i].Conflict;
+        // conflict_ids.push(conflict_string.toString().split("-"));
+    }
+
+    // select no conflict courses based on just ids
+    // TODO: can be improved by using sublist removing!!!
+    let child_nodes = [];
+    let count = 0;
+    for (var i = 0; i < eligible_ids.length; i++) {
+        let course1_id = eligible_ids[i];
+
+        for (var j = i + 1; i < eligible_ids.length; j++) {
+            let course2_id = eligible_ids[j];
+            if (conflict_ids[i].includes(course2_id)) {
+                continue;
+            }
+
+            for (var k = j + 1; k < eligible_ids.length; k++) {
+                let course3_id = eligible_ids[k];
+                if (conflict_ids[i].includes(course3_id)) {
+                    continue;
+                }
+                if (conflict_ids[j].includes(course3_id)) {
+                    continue;
+                }
+
+                let child_course_status = prev_course_status;
+                child_course_status[course1_id] = 1;
+                child_course_status[course2_id] = 1;
+                child_course_status[course3_id] = 1;
+                let child_node = new course_node(child_course_status);
+                child_nodes.push(child_node);
+                count++;
+                if(count > 10){
+                    break;
+                }
+            }
+        }
+    }
+    return child_nodes;
+}
