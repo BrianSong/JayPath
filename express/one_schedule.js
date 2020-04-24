0712ce32b929fbfcbfc34f6e38d5dfa442ace1b7
Simon <Gtingwen@outlook.com>
2020-04-24 14:53:20 +0800

    modified part of one_schedule

--- express/one_schedule.js
+++ express/one_schedule.js
@@ -1,17 +1,21 @@
 var course_node = require("./course_node");
+var grad = require("./check_graduation");
 
 module.exports = {
-    get_schedule: function get_schedule(all_semesters_list) {
+    get_schedule: function get_schedule(all_semesters_list, courseList) {
         // input: a list, where each element (corresponding to a term) is a list of course codes
         // output: a list of course nodes, where each course node is the courses to be taked at each semester
 
         let schedule_list = []; // store the best course nodes for every semesters
         if (all_semesters_list.length == 0) { return schedule_list; }
-        if (all_semesters_list.length == 1) {
-            for (var i = 0; i < all_semesters_list[0].length; i++) {
-                if (all_semesters_list[0][i].get_value == 1) { // must exist one, otherwise all_semesters_list will be []
-                    return [all_semesters_list[0][i]];
-                }
+
+        for (var i = 0; i < all_semesters_list[all_semesters_list.length-1].length; i++) {
+            let curr_node = all_semesters_list[all_semesters_list.length - 1][i];
+            let g = grad.check(curr_node.get_status, courseList);
+            if(g){
+                curr_node.change_node_value(1);
+            }else{
+                curr_node.change_node_value(0);
             }
         }
 
@@ -22,8 +26,10 @@ module.exports = {
                 // get indices of child nodes at the next semester
                 let child_indices_list = all_semesters_list[i][j].get_child_indices;
                 let child_values_list = [];
+                let sum = 0;
                 for (var k = 0; k < child_indices_list.length; k++) {
                     child_values_list.push(all_semesters_list[i + 1][child_indices_list[k]].get_value);
+                    sum += all_semesters_list[i + 1][child_indices_list[k]].get_value;
                 }
 
                 // choose the best child
