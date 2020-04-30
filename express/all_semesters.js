f6fa64bdea9550cee5cd38c1486c48bc1080f1b6
Long Wang <wanglong24@gmail.com>
2020-04-30 11:11:09 -0700

    clean all_semester.js

--- express/all_semesters.js
+++ express/all_semesters.js
@@ -1,15 +1,20 @@
+var CourseNode = require("./model/CourseNode");
 const one_semester = require("./one_semester");
-var course_node = require("./course_node");
 const grad = require("./check_graduation");
 
 module.exports = {
+    /**
+     * Get the CourseNode list for all next semester
+     * @param  {List<CourseNode>} user_semester_list The CourseNode list for the user semester
+     * @param  {String}           field              The focus area of the user
+     * @param  {String}           term               The term (Fall or Spring) of the current semester
+     * @param  {Integer}          semesters_left     The number of semesters left
+     * @return {{List<List<CourseNode>>}             The list of CourseNode list for all the semesters
+     */
     get_all_semesters: function get_all_semesters(user_semester_list, field, term, semesters_left) {
-        // input: user course nodes list (a list contains a single course node)
-        // output: a list where each element (corresponding to a term) is a list of course codes
-
-        let all_semesters = []; // every element is a list of course nodes
-        let curr_semester_nodes_list = [];
-
+        let all_semesters = []; // store the CourseNode list for all the semesters
+        
+        let curr_semester_nodes_list = []; // the CourseNode list for the current semester
         while (all_semesters.length < semesters_left) {
             // generate the next semester nodes based on the previous semester nodes
             if (all_semesters.length == 0) { // use user semester if generating the first semester
@@ -21,11 +26,8 @@ module.exports = {
             // exit if no more courses to take
             if (curr_semester_nodes_list.length == 0) {
                 console.log("no more courses to take, exiting...");
-                // let keys = all_semesters[all_semesters.length - 1][0].get_status.values();
                 let testNodeList = [all_semesters[all_semesters.length - 1][0]];
                 console.log(all_semesters[all_semesters.length - 1][0].get_status);
-                // let t = one_semester.get_one_semester(testNodeList, field, term);
-                // console.log("test length: " + t.length);
                 return all_semesters;
             }
 
@@ -33,32 +35,9 @@ module.exports = {
             all_semesters.push(curr_semester_nodes_list);
             
             // change term for generating next semester
-            if (term == "Fall") {
-                term = "Spring";
-            } else {
-                term = "Fall";
-            }
+            if (term == "Fall") { term = "Spring"; }
+            else {term = "Fall";}
         }
-        
         return all_semesters;
-
-        // ONLY FOR TESTING: set values for all course nodes at the last semester to 1
-        // for (var i = 0; i < all_semesters[all_semesters.length - 1].length; i++) {
-        //     all_semesters[all_semesters.length - 1][i].change_node_value(1);
-        //     grad_flag = true;
-        // }
-
-        // if (grad_flag) {
-        //     return all_semesters; // note all the nodes should have values assinged, but only the values at the last semester matters
-        // } else {
-        //     return []; // cannot graduate, dump everything
-        // }
     }
 };
-
-// function check_grad(nodes_list, field, courses_track, courses_pre) {
-//     for (var i = 0; i < nodes_list.length; i++) {
-//         nodes_list[i].change_node_value(0);
-//     }
-//     return false;
-// }
